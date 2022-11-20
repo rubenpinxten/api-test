0400c10e8b7370f45877ed32bbb17c14bdd0b594
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2022-11-20 16:03:54 +0100

    test

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 import json
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://localhost.tiangolo.com",
+    "http://127.0.0.1:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 quotes = [{"quote": "Het leven is niet een kwestie van de beste kaarten hebben, maar eerder een slechte hand op de juiste manier weten te spelen."},
 {"quote": "Het beste bewijs van liefde is vertrouwen."}, 
