f903f6f8fd2421fa37a23ea63a027705e647a618
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2022-11-20 11:54:15 +0100

    test

--- app/main.py
+++ app/main.py
@@ -1,33 +1,36 @@
 from fastapi import FastAPI
-import json
+#import json
 from pydantic import BaseModel
 
 app = FastAPI()
 
-application = {}
+#application = {}
 
-with open('quotes.json') as csv_file:
-    csv_file.readline()
+#with open('quotes.json') as csv_file:
+    #csv_file.readline()
 
-    line = csv_file.readline()
-    while line:
-        application[line]
-        line = csv_file
+    #line = csv_file.readline()
+    #while line:
+        #application[line]
+        #line = csv_file
 
-class Tekst(BaseModel):
-    quote: str
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
-@app.get("/quote/first")
-async def get_first_quote():
-    return application[0]
+#@app.get("/quote/first")
+#async def get_first_quote():
+ #   return application[0]
 
-@app.get("/quote/last")
-async def get_last_quote():
-    return application[-1]
+#@app.get("/quote/last")
+#async def get_last_quote():
+  #  return application[-1]
 
-@app.post("/teksts/")
-async def post_new_quote(tekst: Tekst):
+@app.post("/items/")
+async def post_new_quote(item: Item):
     #application = application + quote 
     #with open('quotes.json', mode='w', encoding='utf-8') as csv_file:
         #json.dump(quote, csv_file)
-    return tekst
+    return item
