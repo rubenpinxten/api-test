61cbcae8a906ebf721b0a53e0d64a095c73fff19
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2022-11-20 11:06:54 +0100

    test

--- app/main.py
+++ app/main.py
@@ -15,9 +15,9 @@ with open('quotes.json') as csv_file:
 
 @app.get("/quote/first")
 async def get_first_quote():
-    return quotes[0]
+    return application[0]
 
 @app.get("/quote/last")
 async def get_last_quote():
-    return quotes[-1]
+    return application[-1]
 
