480617281bf5937651aee11a80f5aeebe08fb722
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2022-11-20 11:26:53 +0100

    test

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ async def get_last_quote():
 
 @app.post("/quotes/")
 def new_quote(quote: Quote) -> Quote:
-    application = application + quote 
-    with open('quotes.json', mode='w', encoding='utf-8') as csv_file:
-        json.dump(new, csv_file)
+    #application = application + quote 
+    #with open('quotes.json', mode='w', encoding='utf-8') as csv_file:
+        #json.dump(new, csv_file)
     return quote
