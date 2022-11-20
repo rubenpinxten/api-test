ff7a3d1d3948f5c1418a8e7bd1b09c1bc8e7735e
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2022-11-20 11:23:34 +0100

    test

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ with open('quotes.json') as csv_file:
         application[line]
         line = csv_file
 
-class New(BaseModel):
+class Quote(BaseModel):
     quote: str
 
 @app.get("/quote/first")
@@ -25,9 +25,9 @@ async def get_first_quote():
 async def get_last_quote():
     return application[-1]
 
-@app.post("/quote/new/")
-async def new_quote(new: New):
-    application = application + new 
+@app.post("/quotes/")
+def new_quote(quote: Quote) -> Quote:
+    application = application + quote 
     with open('quotes.json', mode='w', encoding='utf-8') as csv_file:
         json.dump(new, csv_file)
-    return new
+    return quote
