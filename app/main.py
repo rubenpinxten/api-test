142f47b83effed504d45c6500c6900e4040cbce6
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2022-11-20 11:05:59 +0100

    test

--- app/main.py
+++ app/main.py
@@ -1,19 +1,17 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
-quotes = [
-        {"Het leven is niet een kwestie van de beste kaarten hebben, maar eerder een slechte hand op de juiste manier weten te spelen."},
-        {"Het beste bewijs van liefde is vertrouwen."},
-        {"De beste vriend zal waarschijnlijk de beste echtgenote krijgen, omdat een goed huwelijk op een talent voor vriendschap berust."},
-        {"Geduld id het beste gebed."},
-        {"De beste manier om iets te leren is er les in te geven."},
-        {"Water is het beste."},
-        {"Tegenslag is de beste gelegenheid om te tonen dan men karakter heeft."},
-        {"Als je voor elke positie de beste speler kiest, heb je nog geen sterk elftal maar een team dat als los zand uiteen valt."},
-        {"De beste manier om zich op iemand te wreken is niet op hem te gelijken."},
-        {"Opleiding is de beste proviand op de reis naar de ouderdom."}
-        ]
+application = {}
+
+with open('quotes.json') as csv_file:
+    csv_file.readline()
+
+    line = csv_file.readline()
+    while line:
+        application[line]
+        line = csv_file
 
 @app.get("/quote/first")
 async def get_first_quote():
