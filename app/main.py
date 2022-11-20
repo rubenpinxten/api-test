62b91babcb929074f081faafd1f4c06aac7396a1
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2022-11-20 17:17:45 +0100

    test

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:8080",
-    "https://localhost.tiangolo.com",
     "http://127.0.0.1:5500"
 ]
 
@@ -19,14 +18,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-quotes = [{"quote": "Het leven is niet een kwestie van de beste kaarten hebben, maar eerder een slechte hand op de juiste manier weten te spelen."},
+quotes = {"quotes": [{"quote": "Het leven is niet een kwestie van de beste kaarten hebben, maar eerder een slechte hand op de juiste manier weten te spelen."},
 {"quote": "Het beste bewijs van liefde is vertrouwen."}, 
 {"quote": "De beste vriend zal waarschijnlijk de beste echtgenote krijgen, omdat een goed huwelijk op een talent voor vriendschap berust."}, 
 {"quote": "Geduld id het beste gebed."}, {"quote": "De beste manier om iets te leren is er les in te geven."}, 
 {"quote": "Water is het beste."}, {"quote": "Tegenslag is de beste gelegenheid om te tonen dan men karakter heeft."}, 
 {"quote": "Als je voor elke positie de beste speler kiest, heb je nog geen sterk elftal maar een team dat als los zand uiteen valt."}, 
 {"quote": "De beste manier om zich op iemand te wreken is niet op hem te gelijken."}, 
-{"quote": "Opleiding is de beste proviand op de reis naar de ouderdom."}]
+{"quote": "Opleiding is de beste proviand op de reis naar de ouderdom."}]}
 
 quote = {}
 
